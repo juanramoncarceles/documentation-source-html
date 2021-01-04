dfb214b62c5a580ab521d01727ee082144907347
ramon <juanramoncarceles@gmail.com>
2021-01-04 18:44:29 +0100

    feat(index state): index collapsed state is preserved
    - The collapsed state of the items in the index tree is preserved
      between page navigations and when changing the site language.
    - To achieve this the state has been moved to a context component.
    - Also each index item has now a unique id, which is the same for
      all the translations of each index item.

--- src/components/index.js
+++ src/components/index.js
@@ -1,75 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
+
+import { useIndexTree } from "../contexts/IndexTreeContext";
 
 import IndexItem from "./indexItem";
 
 const Index = ({ lang, cssClasses }) => {
-  // GraphQL requires to be explicit about levels of nested data.
-  // So if more levels are added to the docs this query should be updated.
-  // It could be written nicer by creating an item type like here:
-  // https://github.com/graphql/graphql-spec/issues/91#issuecomment-254895093
-  // with this: https://www.gatsbyjs.com/docs/schema-customization/
-  const {
-    allDocsIndex: { nodes: indexes },
-  } = useStaticQuery(
-    graphql`
-      query allIndexQuery {
-        allDocsIndex {
-          nodes {
-            lang
-            items {
-              label
-              originalFile
-              url
-              items {
-                label
-                originalFile
-                url
-                items {
-                  label
-                  originalFile
-                  url
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  );
-
-  // TODO everything related to the items ids would be better in gatsby-node so it can be just taken from the query.
-  // Processes an index array to add the id to each index item (index should be the same for langs).
-  const addChainedIdToIndexItems = (array, parentLabel = "") => {
-    for (let i = 0; i < array.length; i++) {
-      const id = parentLabel + array[i].originalFile.toLowerCase();
-      array[i].id = id;
-      if (array[i].items) {
-        addChainedIdToIndexItems(array[i].items, id);
-      }
-    }
-  };
-
-  indexes.forEach(index => {
-    addChainedIdToIndexItems(index.items);
-  });
-
-  const createIndexCollapsedStateObj = (outObj, array, parentLabel = "") => {
-    for (let i = 0; i < array.length; i++) {
-      const id = parentLabel + array[i].originalFile.toLowerCase();
-      outObj[id] = true;
-      if (array[i].items) {
-        createIndexCollapsedStateObj(outObj, array[i].items, id);
-      }
-    }
-  };
-
-  const [collapsementState, setCollapsementState] = useState(() => {
-    const obj = {};
-    createIndexCollapsedStateObj(obj, indexes[0].items);
-    return obj;
-  });
+  const { indexes, collapsementState, setCollapsementState } = useIndexTree();
 
   const getIndexDataByLang = lang => {
     const indexObj = indexes.find(index => index.lang === lang);
---
+++ src/contexts/IndexTreeContext.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect, useContext, createContext } from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
+const IndexTreeContext = createContext();
+
+const IndexTreeContextProvider = ({ children }) => {
+  // GraphQL requires to be explicit about levels of nested data.
+  // So if more levels are added to the docs this query should be updated.
+  // It could be written nicer by creating an item type like here:
+  // https://github.com/graphql/graphql-spec/issues/91#issuecomment-254895093
+  // with this: https://www.gatsbyjs.com/docs/schema-customization/
+  const {
+    allDocsIndex: { nodes: indexes },
+  } = useStaticQuery(
+    graphql`
+      query allIndexQuery {
+        allDocsIndex {
+          nodes {
+            lang
+            items {
+              label
+              originalFile
+              url
+              items {
+                label
+                originalFile
+                url
+                items {
+                  label
+                  originalFile
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+
+  // TODO useEffect to set/get in localStorage the state of the index?
+
+  // TODO everything related to the index items ids would be better to be handled in gatsby-node
+  // so it can be added to the nodes, and here it would be just taken from the query.
+
+  // Processes an index array to add the id to each index item.
+  // Index strucutre and original file names should be the same for all languages.
+  const addChainedIdToIndexItems = (array, parentLabel = "") => {
+    for (let i = 0; i < array.length; i++) {
+      const id = parentLabel + array[i].originalFile.toLowerCase();
+      array[i].id = id;
+      if (array[i].items) {
+        addChainedIdToIndexItems(array[i].items, id);
+      }
+    }
+  };
+
+  indexes.forEach(index => {
+    addChainedIdToIndexItems(index.items);
+  });
+
+  // Creates an object with all the id of the items as keys and a boolen indicating
+  // its collapsed state as value.
+  const createIndexCollapsedStateObj = (outObj, array, parentLabel = "") => {
+    for (let i = 0; i < array.length; i++) {
+      const id = parentLabel + array[i].originalFile.toLowerCase();
+      outObj[id] = true;
+      if (array[i].items) {
+        createIndexCollapsedStateObj(outObj, array[i].items, id);
+      }
+    }
+  };
+
+  const [collapsementState, setCollapsementState] = useState(() => {
+    const obj = {};
+    createIndexCollapsedStateObj(obj, indexes[0].items);
+    return obj;
+  });
+
+  return (
+    <IndexTreeContext.Provider
+      value={{ indexes, collapsementState, setCollapsementState }}
+    >
+      {children}
+    </IndexTreeContext.Provider>
+  );
+};
+
+const useIndexTree = () => {
+  const context = useContext(IndexTreeContext);
+  if (context === undefined) {
+    throw new Error("useIndexTree must be within a IndexTreeContextProvider");
+  }
+  return context;
+};
+
+export { IndexTreeContextProvider, useIndexTree };
