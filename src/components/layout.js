9ddec1df243a31a84797d193cd63998fdd96c6c1
ramon <juanramoncarceles@gmail.com>
2021-01-13 11:47:42 +0100

    Renamed css file with the styles for the source contents.

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 
-import "../styles/LandsDesign.css";
+import "../styles/content-area.css";
 
 import Header from "./header";
 
