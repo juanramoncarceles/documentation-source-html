4e9215453f76e6b2a4c5b3d613d2d2be6838a624
ramon <juanramoncarceles@gmail.com>
2021-01-13 09:22:21 +0100

    Removed unnecessary namespace attr on an inline svg.

--- src/components/indexItem.js
+++ src/components/indexItem.js
@@ -24,7 +24,6 @@ const IndexItem = ({ item, lang }) => {
               viewBox="0 0 24 24"
               height="1.4em"
               width="1.4em"
-              xmlns="http://www.w3.org/2000/svg"
             >
               {open ? (
                 <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z" />
