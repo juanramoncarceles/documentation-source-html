8a4521f7beb4821adf182f94a20aa31ea9526474
ramon <juanramoncarceles@gmail.com>
2021-01-22 18:33:51 +0100

    Added link to Asuni on the footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="p-3 text-center border-t border-gray-200">
-      © {new Date().getFullYear()}, Asuni Soft
+      <span>
+        © {new Date().getFullYear()},{" "}
+        <a href="https://www.asuni.com/">Asuni Soft</a>
+      </span>
     </footer>
   );
 };
